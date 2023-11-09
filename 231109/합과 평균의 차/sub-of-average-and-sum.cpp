e4440e85bcb62bf521a4da90fd3c59f7e4786ed0
Hyeonseob Lee <97226053+LeeHyeonseob@users.noreply.github.com>
2023-11-09 20:54:05 +0900

    Title: 합과 평균의 차, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231109/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a,b,c;
+
+    cin >> a >> b>> c;
+
+    int sum = a+b+c;
+    cout << sum << endl;
+    cout << sum / 3 << endl;
+    cout << sum - (sum / 3);
+    return 0;
+}
