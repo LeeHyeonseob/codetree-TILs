b6ecc8d67f46e8fb9973320ccef51bb22d700475
Hyeonseob Lee <97226053+LeeHyeonseob@users.noreply.github.com>
2023-11-11 17:15:36 +0900

    Title: a의 배수들의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231111/a의 배수들의 곱/product-of-multiples-of-a.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a,b;
+    cin >> a >> b;
+    int mul = 1;
+
+    for(int i=1; i<=b; i++){
+        if(i % a == 0){
+            mul *= i;
+        }
+    }
+    cout << mul;
+    
+    return 0;
+}
