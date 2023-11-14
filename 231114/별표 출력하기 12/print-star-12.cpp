125a2e4fd4f4b098dd43e4d5c86732997910055f
Hyeonseob Lee <97226053+LeeHyeonseob@users.noreply.github.com>
2023-11-14 14:59:41 +0900

    Title: 별표 출력하기 12, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231114/별표 출력하기 12/print-star-12.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(j % 2 != 0){
+                if(i<=j){
+                    cout << "* ";
+                }
+                else{
+                    cout << "  ";
+                }
+            }
+            else if(j % 2 == 0){
+                if(i == 0){
+                    cout << "* ";
+                }
+                else{
+                    cout << "  ";
+                }
+            }
+        }
+        cout << "\n";
+    }
+
+
+    return 0;
+}
